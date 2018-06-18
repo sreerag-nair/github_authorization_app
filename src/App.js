0cbf2c103c0ca6e91c81ff58dc481889fd75b7d3
SreeraG <sreerag.nair@built.io>
2018-06-18 16:19:29 +0530

    LocalStrategy testing done; GitHubStrategy works for now

--- src/App.js
+++ src/App.js
@@ -2,29 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 import Dashboard from './Dashboard';
-import { Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 const passport = require('passport')
 const cors = require('cors')
 class App extends Component {
-  
-  
-  state = {
-    emailId: '',
-    password: ''
-  }
-  
+
   updateEmail(event) {
     console.log(" email value : ", event.target.value)
     this.setState({ emailId: event.target.value })
-    
+
   }
-  
+
   updatePassword(event) {
     console.log(" pwd value : ", event.target.value)
     this.setState({ password: event.target.value })
   }
-  
-  
+
+
   contactGithub() {
     // console.log("HERE<")
     //  axios.get('https://www.github.com/login/oauth/authorize/',{
@@ -40,18 +34,18 @@ class App extends Component {
     // .then((response) =>{
     //   console.log("response from github : ", response)
     // }) 
-    
+
     //this doesnt work when the window name is 'gitWindow'.... strange
     // window.open('https://www.github.com/login/oauth/authorize/?client_id=3fb8c782622ac4a1d0a6',
     //   'gitWindow',
     //   "height=100,width=100")
-    
+
     //but this does.....
     window.open('https://www.github.com/login/oauth/authorize/?client_id=3fb8c782622ac4a1d0a6',//&redirect_uri=http://localhost:3000/dashboard',
-    'gitwindow',
-    "height=600,width=500")
-    
-    
+      'gitwindow',
+      "height=600,width=500")
+
+
     /*  fetch('https://www.github.com/login/oauth/authorize/?client_id=3fb8c782622ac4a1d0a6', {
     method: 'GET',
     mode: 'no-cors',
@@ -62,60 +56,83 @@ class App extends Component {
   .then((response) => {
     console.log('rs : ', response)
   }) */
-  
-}
+
+  }
 
 
-//// VERY IMPORTANT FUNCTION////
-check(){
-  const code =
-  window.location.href.match(/\?code=(.*)/) &&
-  window.location.href.match(/\?code=(.*)/)[1]
-  console.log(window.location.href)
-}
+  //// VERY IMPORTANT FUNCTION////
+  check() {
+    const code =
+      window.location.href.match(/\?code=(.*)/) &&
+      window.location.href.match(/\?code=(.*)/)[1]
+    console.log(window.location.href)
+  }
 
-render() {
-  
-  return (
-    <span>
-    <Switch>
-    
-    <Route exact path="/" render={() => {
-      return (
-        <div className="App">
-        <div id="loginBox">
-        <div style={{ paddingTop: '100px' }}>
-        <div>
-        <div>email-id</div>
-        <input type="email" value={this.state.emailId} onChange={this.updateEmail.bind(this)} />
-        </div>
-        <div>
-        <div>password</div>
-        <input type="text" value={this.state.password} onChange={this.updatePassword.bind(this)} />
-        </div>
-        <div>
-        {/* <a href = "">Link</a> */}
-        <button type="submit" style={{ marginTop: '20px' }}>
-        <a href = "https://www.github.com/login/oauth/authorize/?client_id=3fb8c782622ac4a1d0a6&redirect_uri=http://localhost:3000/&allow_signup=false">
-        Login using Github
-        </a>
-        </button>
-        <button onClick = { this.check }>
-        Check!
-        </button>
-        </div>
-        </div>
-        </div>
-        </div>
-      )
-    }} />
-    
-    <Route exact path="/dashboard" component={Dashboard} />
-    
-    </Switch>
-    </span>
-  );
-}
+  localdata(){
+    console.log("dszfgv : ", this.state.emailId)
+    console.log("dsxfgdfh : ", this.state.password)
+
+    axios.post('http://localhost:8001/localstrategy', {
+    username : this.state.emailId,
+    password : this.state.password
+    })
+    .then((response) =>{
+      console.log("response : ", response)
+    })
+  }
+
+
+  state = {
+    emailId: '',
+    password: ''
+  }
+
+  render() {
+
+    return (
+      <span>
+        <Switch>
+
+          <Route exact path="/" render={() => {
+            return (
+              <div className="App">
+                <div id="loginBox">
+                  <div style={{ paddingTop: '100px' }}>
+                    <div>
+                      <div>email-id</div>
+                      <input type="email" required value={this.state.emailId} onChange={this.updateEmail.bind(this)} />
+                    </div>
+                    <div>
+                      <div>password</div>
+                      <input type="text" required value={this.state.password} onChange={this.updatePassword.bind(this)} />
+                    </div>
+                    <div>
+                      {/* <a href = "">Link</a> */}
+
+
+                      {/* <Link to='http://localhost:8001/fire'> */}
+                        <button type="submit" style={{ marginTop: '20px' }}>
+                          <a href = "http://localhost:8001/fire">
+                          Login using Github
+                          </a>
+                        </button>
+                      {/* </Link> */}
+                      <button type = "submit" onClick={this.localdata.bind(this)}>
+                        Submit
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )
+          }} />
+
+          <Route exact path="/dashboard" component={Dashboard} />
+
+        </Switch>
+      </span>
+    );
+  }
 }
 
 export default App;
--- src/server.js
+++ src/server.js
@@ -1,15 +1,97 @@
-import express from 'express';
+const express = require('express');
 const app = express();
 const passport = require('passport')
-const gitStrategy = require('passport-github').Strategy
+const GitHubStrategy = require('passport-github').Strategy
+const body_parser = require('body-parser')
+const LocalStrategy = require('passport-local').Strategy
+
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+    next();
+});
+
+app.use(passport.initialize())
+app.use(body_parser.json())
+
+passport.use(new LocalStrategy({
+    session: false
+}
+    , function (username, password, done) {
+        if(username === "sree" && password === "123" ){
+            done(null,{username : username, password : password})
+        }
+        else{
+            done({error : "ERROR... USERNAME PASSWORD NOT VALID"})
+        }
+    }))
 
 
 
+passport.use('github', new GitHubStrategy({
+    clientID: '3fb8c782622ac4a1d0a6',
+    clientSecret: 'fe62abf8e17b91d71e2c94c367af00e6ba7ba8e6',
+    callbackURL: 'http://localhost:8001/cb',
+    scope: 'user:email',         //the dealbreaker!!!
+},
+    function (accessToken, refreshToken, profile, done) {
 
-app.get('/fire', function(req, res, next){
+        // console.log("accessToken : ", accessToken);
+        // console.log("refreshToken : ", refreshToken);
+        console.log("profile : ", profile);
+        // console.log("done : ", done);
 
+        done(null, profile)
+    }))
+
+
+app.get('/fire', passport.authenticate('github'),
+
+
+    // ,function (req, res, next) {
+
+    //     passport.authenticate('gh', function (req, res, next) {
+
+    // console.log('IN FIRE......')
+
+    //         console.log('req error : ', req.error)
+    //         console.log('req user : ', req.user)
+    //         console.log('req info : ', req.authInfo)
+    //     })(req, res, next);
+
+
+    // }
+)
+
+
+app.post('/localstrategy', function (req, res, next) {
+    passport.authenticate('local',function(err,user,info){
+
+        console.log("err : ", err)
+        console.log("user : ", user)
+        console.log("info : ", info)
+    })(req,res,next)
 })
 
-app.listen(8001, function(){
+app.get('/cb',
+    function (req, res, next) {
+        passport.authenticate('github', function (req, res, next) {
+
+            console.log('HJGFNNYGBF')
+
+            console.log('req error : ', req.error)
+            console.log('req user : ', req.user)
+            console.log('req info : ', req.authInfo)
+
+
+        })(req, res, next);
+
+        res.end();
+    })
+
+app.listen(8001, function () {
     console.log("SERVER RUNNING IN PORT 8001")
 })
+
